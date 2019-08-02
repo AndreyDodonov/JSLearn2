17fed3def974478fa65e4507c58ce60463d82c04
AndreyDodonov <Nordhummer@gmail.com>
2019-08-02 17:14:47 +0300

    + timer function

--- script/script.js
+++ script/script.js
@@ -1,1 +1,23 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+    /* timer */
+    function countTimer(deadline) {
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinute = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds'),
+            dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateNow - dateStop)/ 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor(timeRemaining / 60 / 60);
+            
+        timerHours.textContent = hours;
+        timerMinute.textContent = minutes;
+        timerSeconds.textContent = seconds;    
+            
+    }
+
+    countTimer('01 july 2019');
+
+});
