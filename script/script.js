2e804335e61d5b62b129418e643b02f98a6e5d6c
AndreyDodonov <Nordhummer@gmail.com>
2019-08-04 22:41:04 +0300

    + tabs

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
-    /* Timer */
+    /* TIMER */
 
     function countTimer(deadline) {
         let useGMT = false;
@@ -46,7 +46,7 @@ window.addEventListener('DOMContentLoaded', function () {
     }
     countTimer('5 august 2019');
 
-    /* menu */
+    /* MENU */
 
     const toggleMenu = () => {
         const menu = document.querySelector('menu');
@@ -62,7 +62,7 @@ window.addEventListener('DOMContentLoaded', function () {
     };
     toggleMenu();
 
-    /* popup */
+    /* POPUP */
 
     const togglePopup = () => {
         const popup = document.querySelector('.popup'),
@@ -111,43 +111,78 @@ window.addEventListener('DOMContentLoaded', function () {
         });
     };
 
-    function scrollToTarget(targetSelector){
-        if (targetSelector.length === 1 || document.querySelector(targetSelector) == null ){
+    function scrollToTarget(targetSelector) {
+        if (targetSelector.length === 1 || document.querySelector(targetSelector) == null) {
             return;
         }
         const targetY = document.querySelector(targetSelector).getBoundingClientRect().top;
-        const startY =  window.pageYOffset;
+        const startY = window.pageYOffset;
         const totalDiffY = Math.abs(targetY - startY);
         const startTime = Date.now();
         const dir = targetY > startY ? 1 : -1;
         const pxInMs = 2;
-    
-        const animationFrame= function(){
+
+        const animationFrame = function () {
             const diffTime = Date.now() - startTime;
             const diffY = diffTime * pxInMs;
             const y = startY + dir * diffY;
-            window.scrollTo(0,y);
-            if ( diffY < totalDiffY){
+            window.scrollTo(0, y);
+            if (diffY < totalDiffY) {
                 requestAnimationFrame(animationFrame);
             } else {
                 window.scrollTo(0, targetY);
             }
         };
-        requestAnimationFrame(animationFrame); 
-    }   
-    
-    document.addEventListener('click',(event)=>{
+        requestAnimationFrame(animationFrame);
+    }
+
+    document.addEventListener('click', (event) => {
         let target = event.target.closest('a');
-        if (target && target.getAttribute('href').startsWith('#')){
+        if (target && target.getAttribute('href').startsWith('#')) {
             event.preventDefault();
             scrollToTarget(target.getAttribute('href'));
         }
+
+        const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]'));
     });
 
 
 
     togglePopup();
 
+    /* TABS */
+
+    const tabs = () => {
+        const tabHeader = document.querySelector('.service-header'),
+              tab = document.querySelectorAll('.service-header-tab'),
+              tabContent = document.querySelectorAll('.service-tab');
+
+        const toggleTabContent = (index) => {
+            for (let i = 0; i < tabContent.length; i++) {
+                if (index === i) {
+                    tab[i].classList.add('active');
+                    tabContent[i].classList.remove('d-none');
+                } else {
+                    tab[i].classList.remove('active');
+                    tabContent[i].classList.add('d-none');
+                }
+            }
+        };
+
+        tabHeader.addEventListener('click', (event) => {
+            let target = event.target;
+            target = target.closest('.service-header-tab');
+            if (target) {
+                tab.forEach((item, i) => {
+                    if (item === target) {
+                        toggleTabContent(i);
+                    }
+                });
+            }
+        });
+    };
+
+    tabs();
 
 
 
